e298eff5e3d89a70aa36fa510d3cda32d1ed8fb0
Dunned <eduardo.jauregui1@unmsm.edu.pe>
2023-01-19 23:15:07 -0500

    Login Primera Version

---
+++ app/src/main/java/com/eduardojr/app_taxis_cliente/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.eduardojr.app_taxis_cliente
+
+import android.app.ActionBar.LayoutParams
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+    }
+}
