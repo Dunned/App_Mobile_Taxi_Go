6126cb98fc3331eddb167b5587933b0c4f8f48cb
Dunned <eduardo.jauregui1@unmsm.edu.pe>
2023-01-20 23:09:03 -0500

    vista registro

--- app/src/main/java/com/eduardojr/app_taxis_cliente/MainActivity.kt
+++ app/src/main/java/com/eduardojr/app_taxis_cliente/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.eduardojr.app_taxis_cliente
 
 import android.app.ActionBar.LayoutParams
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.WindowManager
+import com.eduardojr.app_taxis_cliente.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        binding.btnRegister.setOnClickListener {
+            goToRegister()
+        }
     }
+
+    private fun goToRegister(){
+        val i = Intent(this, RegisterActivity::class.java)
+        startActivity(i)
+    }
+
+
 }
